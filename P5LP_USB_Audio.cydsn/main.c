6ca28ab76993fef342657ddc819be705b13c19f1
noeldiviney <noel@ayrenet.com>
2017-12-18 13:40:57 +1100

    updating

--- P5LP_USB_Audio.cydsn/main.c
+++ P5LP_USB_Audio.cydsn/main.c
@@ -17,6 +17,18 @@ int main(void)
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
+   UART_dbg_Start();
+
+   I2C_M_FF_Start();
+   I2C_S_PCF8574_Start();
+    
+   TinyPrintf_Start();
+   
+   printf("%s", "Hello World");
+   NewLine();
+   TriggerScope();
+   I2C_S_PCF8574_Test();    
+    
     for(;;)
     {
         /* Place your application code here. */
